3a0c3bed3fcf86251df4c8f6e9cfb5f1559303b1
Felipe Salgado <felipe.salgado@civicplus.com>
2019-08-28 08:28:48 -0500

    fix react useEffect warning

--- src/components/MessagesList/index.js
+++ src/components/MessagesList/index.js
@@ -4,10 +4,13 @@ import services from '../../services';
 
 const MessagesList = () => {
     const [messages, setMessages] = useState([]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(async () => {
-        const data = await services.getMessages();
-        setMessages(data);
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await services.getMessages();
+            setMessages(data);
+        };
+
+        fetchData();
     }, []);
     return (
         <div>
