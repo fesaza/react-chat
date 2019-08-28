efdce8a6c32c8eb510314607ed9a999a8273c9f3
Felipe Salgado <felipe.salgado@civicplus.com>
2019-08-28 08:24:43 -0500

    send message to BE

--- src/components/SendMessageToolbar/index.js
+++ src/components/SendMessageToolbar/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import services from '../../services';
 
 const SendMessageToolbar = () => {
+    const [message, setMessage] = useState();
     return (
         <div>
-            Enviar mensaje
+            <input type="text" placeholder="type a message" onChange={(e) => {
+                setMessage(e.target.value);
+            }} />
+            <button onClick={() => {
+                if(message){
+                    services.sendMessage(message, "fesaza");
+                }
+            }}>Send</button>
         </div>
     )
 }
--- src/services.js
+++ src/services.js
@@ -5,7 +5,13 @@ const url = 'https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0/';
 
 const services = {
     getMessages: () => axios.get(`${url}?token=${token}`).then(result => result.data),
-    sendMessage: () => {},
+    sendMessage: (message, author) => axios.post(url, {
+        message, author
+    }, {
+        headers: {
+            token
+        }
+    }),
 };
 
 export default services;
